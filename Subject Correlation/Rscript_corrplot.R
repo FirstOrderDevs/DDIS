ad5fc11cfd83ce9c397adceff9c9bc0c776e0d2d
Wishmitha <wishmitha.14@cse.mrt.ac.lk>
2018-09-13 19:32:34 +0530

    improve for all three correlation types

--- Subject Correlation/Rscript_corrplot.R
+++ Subject Correlation/Rscript_corrplot.R
@@ -1,28 +1,33 @@
-install.packages('corrplot', repos='http://cran.us.r-project.org')
-install.packages('Hmisc', repos='http://cran.us.r-project.org')
+#install.packages('corrplot')
+#install.packages('Hmisc', repos='http://cran.us.r-project.org')
+#install.packages('ggpubr', repos='http://cran.us.r-project.org')
 
 
-grade_6 <- read.csv("/home/adeesha/Documents/Research/grade_6_performance.csv")
-grade_7 <- read.csv("/home/adeesha/Documents/Research/grade_7_performance.csv")
-grade_8 <- read.csv("/home/adeesha/Documents/Research/grade_8_performance.csv")
+grade_6 <- read.csv("marks_mean_grade_6.csv")
+grade_7 <- read.csv("marks_mean_grade_7.csv")
+grade_8 <- read.csv("marks_mean_grade_8.csv")
 
-library("Hmisc")
 
-res_6 <- rcorr(as.matrix(grade_6))
-res_7 <- rcorr(as.matrix(grade_7))
-res_8 <- rcorr(as.matrix(grade_8))
+grade_6 <- subset(grade_6, select = -X)
+grade_7 <- subset(grade_7, select = -X)
+grade_8 <- subset(grade_8, select = -X)
+
+
+res_6 <- round(cor(grade_6, use="complete.obs", method="kendall"),2)
+res_7 <- round(cor(grade_7, use="complete.obs", method="kendall"),2)
+res_8 <- round(cor(grade_8, use="complete.obs", method="kendall"),2)
 
 library(corrplot)
+col_pallet=colorRampPalette(c("red","pink","lightpink","lightblue","blue"))(200)
 
 pdf("grade_6_correlation.pdf")
-corrplot(res_6$r, type="upper", order="alphabet",tl.col="black",cl.lim=c(0.4,1),is.corr=FALSE,title="Grade 6",p.mat = res_6$P,sig.level = 0.01, insig = "blank",mar=c(0,0,2,0))
+corrplot(res_6, method="color",type="upper", number.cex = .7,addCoef.col = "black", order="alphabet",tl.col="black",cl.lim=c(0,1),col=col_pallet,is.corr=FALSE,title="Grade 6",mar=c(0,0,2,0))
 dev.off()
 
 pdf("grade_7_correlation.pdf")
-corrplot(res_7$r, type="upper", order="alphabet",tl.col="black",cl.lim=c(0.4,1),is.corr=FALSE,title="Grade 7",p.mat = res_7$P,sig.level = 0.01, insig = "blank",mar=c(0,0,2,0))
+corrplot(res_7, method="color", type="upper", number.cex = .7,addCoef.col = "black", order="alphabet",tl.col="black",cl.lim=c(0,1),col=col_pallet,is.corr=FALSE,title="Grade 7",mar=c(0,0,2,0))
 dev.off()
 
 pdf("grade_8_correlation.pdf")
-corrplot(res_8$r, type="upper", order="alphabet",tl.col="black",cl.lim=c(0.4,1),is.corr=FALSE,title="Grade 8",p.mat = res_8$P,sig.level = 0.01, insig = "blank",mar=c(0,0,2,0))
+corrplot(res_8,method="color", type="upper", number.cex = .7,addCoef.col = "black", order="alphabet",tl.col="black",cl.lim=c(0,1),col=col_pallet,is.corr=FALSE,title="Grade 8",mar=c(0,0,2,0))
 dev.off()
-
